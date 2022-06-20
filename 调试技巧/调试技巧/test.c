a08b02b67d2110754d1448d2acb342163ecff947
xin-xiangrong <1005037512@qq.com>
2022-06-20 22:59:29 +0800

    调试

---
+++ 调试技巧/调试技巧/test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char* p = "hello world";
+//	printf("%s\n", p);
+//
+//	return 0;
+//}
+// 
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9.,10 };
+
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}
+
+	return 0;
+}
+
+//#include<stdio.h>
+//
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//
+//void test1()
+//{
+//	test2();
+//}
+//
+//void test()
+//{
+//	test1();
+//	return 0;
+//}
+//
+//int main()
+//{
+//	test();
+//}
