d02c874f9ac05a285a5b0dc1002bb5240783b517
xin-xiangrong <1005037512@qq.com>
2022-06-21 23:15:52 +0800

    6_21

---
+++ strcpy和strlen模拟/strcpy和strlen模拟/test.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////strcpyÄ£Äâ
+//#include<stdio.h>
+//#include<assert.h>
+//
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	//¶ÏÑÔ
+//	assert(src != NULL);
+//	assert(dest != NULL);
+//
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "hello world";
+//
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	return 0;
+//}
+
+//strlenÄ£Äâ
+#include<stdio.h>
+#include<assert.h>
+
+unsigned int my_strlen(const char* str)
+{
+	int count = 0;
+	assert(str);
+	while (*str != '\0')
+	{
+		count++;;
+		str++;
+	}
+	return count;
+}
+
+int main()
+{
+	char arr[] = "hello world";
+
+	printf("%u\n", my_strlen(arr));
+	return 0;
+}
