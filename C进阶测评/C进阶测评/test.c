45038a34cc5aac1663a28e00c011973f5d04592c
xin-xiangrong <1005037512@qq.com>
2022-07-26 11:57:29 +0800

    练习

--- C进阶测评/C进阶测评/test.c
+++ C进阶测评/C进阶测评/test.c
@@ -34,3 +34,39 @@ int main()
     }
     return 0;
 }
+
+//替换空格
+//请实现一个函数，将一个字符串中的每个空格替换成“ % 20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为
+//We % 20Are % 20Happy。
+class Solution {
+public:
+    void replaceSpace(char* str, int length) {
+        char* cur = str;
+        int space_count = 0;
+        while (*cur)
+        {
+            if (*cur == ' ')
+            {
+                space_count++;
+            }
+            cur++;
+        }
+        int end1 = length - 1;
+        int end2 = length - 1 + 2 * space_count;
+        while (end1 != end2)
+        {
+            if (str[end1] != ' ')
+            {
+                str[end2--] = str[end1--];
+            }
+            else
+            {
+                end1--;
+                str[end2--] = '0';
+                str[end2--] = '2';
+                str[end2--] = '%';
+            }
+        }
+
+    }
+};
